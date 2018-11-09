206eb21bf00cae3dcd861eff1f6271a5b0721316
Iona Buchanan <iona.buchanan10@gmail.com>
2018-11-09 16:17:04 -0500

    add book a job form

--- Service4U/app/src/main/java/ca/uottawa/service4u/BookJob.java
+++ Service4U/app/src/main/java/ca/uottawa/service4u/BookJob.java
@@ -2,6 +2,11 @@ package ca.uottawa.service4u;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -12,6 +17,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -20,9 +26,20 @@ public class BookJob extends AppCompatActivity {
 
     Calendar calendar = Calendar.getInstance();
 
+    private Spinner serviceSpinner;
+    private Spinner urgencySpinner;
+    private Spinner timeSpinner;
+
     FirebaseDatabase database;
     DatabaseReference databaseUsers;
+    DatabaseReference databaseServices;
     List<ServiceProvider> serviceProviders;
+    List<Service> allServices;
+    List<String> serviceNames;
+    ArrayAdapter<CharSequence> serviceAdapter;
+    Service currentService;
+    int currentUrgency;
+    double timeLength;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +47,9 @@ public class BookJob extends AppCompatActivity {
         setContentView(R.layout.activity_book_job);
 
         database = FirebaseDatabase.getInstance();
+
+        serviceProviders = new ArrayList<ServiceProvider>();
         databaseUsers = database.getReference("Users");
-
-
         databaseUsers.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -51,6 +68,117 @@ public class BookJob extends AppCompatActivity {
 
             }
         });
+
+
+        //Service Type spinner
+        allServices = new ArrayList<Service>();
+        serviceNames = new ArrayList<String>();
+        serviceSpinner = findViewById(R.id.serviceSpinner);
+
+        serviceAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, serviceNames);
+        serviceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        serviceSpinner.setAdapter(serviceAdapter);
+
+        serviceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Log.v("current service", serviceNames.get(position));
+                currentService = allServices.get(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.v("item", "none");
+            }
+        });
+
+        databaseServices = database.getReference("Services");
+        databaseServices.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                allServices.clear();
+                serviceNames.clear();
+
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                    Service s = postSnapshot.getValue(Service.class);
+                    allServices.add(s);
+
+                }
+
+                //sort services by type and by name
+                Collections.sort(allServices, (Service s1, Service s2) ->{
+                    String str1 = String.format("%s - %s", s1.getType(), s1.getName());
+                    String str2 = String.format("%s - %s", s2.getType(), s2.getName());
+                    return str1.compareToIgnoreCase(str2);
+                });
+
+                for (Service s : allServices){
+                    serviceNames.add(String.format("%s - %s", s.getType(), s.getName()));
+                }
+
+                //update
+                serviceAdapter = new ArrayAdapter(BookJob.this,android.R.layout.simple_spinner_item, serviceNames);
+                serviceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                serviceSpinner.setAdapter(serviceAdapter);
+                currentService = allServices.get(0);
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        //urgency spinner
+        urgencySpinner = findViewById(R.id.urgencySpinner);
+        ArrayAdapter<CharSequence> urgencyAdapter = ArrayAdapter.createFromResource(this,
+                R.array.urgency_array, android.R.layout.simple_spinner_item);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        urgencyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        urgencySpinner.setAdapter(urgencyAdapter);
+        currentUrgency = 0;
+        urgencySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Log.v("urgency", (String) parent.getItemAtPosition(position));
+                currentUrgency = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.v("item", "none");
+            }
+        });
+
+
+        //urgency spinner
+        timeSpinner = findViewById(R.id.timeSpinner);
+        List<String> timeList = new ArrayList<String>();
+        for (int i=0; i < 5; i++){
+            timeList.add(String.valueOf(i*0.5));
+        }
+        ArrayAdapter<CharSequence> timeAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, timeList);
+        timeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeSpinner.setAdapter(timeAdapter);
+        timeLength = 0.5;
+        timeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Log.v("time length", (String) parent.getItemAtPosition(position));
+                timeLength = (position+1)*0.5;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.v("item", "none");
+            }
+        });
+
     }
 
     public Date addDays(Date date, int n) {
@@ -119,5 +247,9 @@ public class BookJob extends AppCompatActivity {
         }
 
         return options;
+    }
+
+    public void handleForm(View view){
+        //TODO
     }
 }
