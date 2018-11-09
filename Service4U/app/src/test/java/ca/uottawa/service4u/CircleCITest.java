8dea1c00b61f68a5a87e08670d2c9826899b9a7e
Victor Teixeira <vteix078@uottawa.ca>
2018-11-09 14:34:51 -0500

    Added second test to create user and assertEquals for firstName

--- Service4U/app/src/test/java/ca/uottawa/service4u/CircleCITest.java
+++ Service4U/app/src/test/java/ca/uottawa/service4u/CircleCITest.java
@@ -14,4 +14,9 @@ public class CircleCITest {
         Service aS=new Service("1", "Virus Removal","IT", 15.5);
         assertEquals("Checking id", "1", aS.getId());
     }
+
+    public void userTest(){
+        User u1=new User("John","Smith", "customer", "1-234-567-8910", "23 lane road");
+        assertEquals("Checking user's firstname", "John", u1.getfirstName());
+    }
 }
