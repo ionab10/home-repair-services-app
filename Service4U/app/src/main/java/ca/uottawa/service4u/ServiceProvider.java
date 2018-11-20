a4c30c93177a51e8ec209b2ba399065639a257a6
Iona Buchanan <iona.buchanan10@gmail.com>
2018-11-20 16:21:31 -0500

    added public types to ServiceProvider vars

--- Service4U/app/src/main/java/ca/uottawa/service4u/ServiceProvider.java
+++ Service4U/app/src/main/java/ca/uottawa/service4u/ServiceProvider.java
@@ -2,21 +2,23 @@ package ca.uottawa.service4u;
 
 import android.icu.util.DateInterval;
 import android.os.Parcelable;
+import android.support.annotation.Keep;
 
 import java.sql.Time;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+@Keep
 public class ServiceProvider extends User {
 
-    List<Service> services;
-    List<TimeInterval> availability;
-    List<TimeInterval> booked;
-    String companyName;
-    double rating;
-    String description;
-    boolean licensed;
+    public List<Service> services;
+    public List<TimeInterval> availability;
+    public List<TimeInterval> booked;
+    public String companyName;
+    public double rating;
+    public String description;
+    public boolean licensed;
 
     public ServiceProvider(String id,
                            String firstName,
@@ -54,6 +56,61 @@ public class ServiceProvider extends User {
 
     }
 
+    public List<Service> getServices() {
+        return services;
+    }
+
+    public void setServices(List<Service> services) {
+        this.services = services;
+    }
+
+    public List<TimeInterval> getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(List<TimeInterval> availability) {
+        this.availability = availability;
+    }
+
+    public List<TimeInterval> getBooked() {
+        return booked;
+    }
+
+    public void setBooked(List<TimeInterval> booked) {
+        this.booked = booked;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isLicensed() {
+        return licensed;
+    }
+
+    public void setLicensed(boolean licensed) {
+        this.licensed = licensed;
+    }
 
     public long available(List<TimeInterval> homeownerAvailability, double hours){
 
